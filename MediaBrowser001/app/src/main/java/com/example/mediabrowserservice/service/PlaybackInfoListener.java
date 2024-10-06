4146eab578fdafbbb294b462e893f4756742bc67
jameel <android2@theinteractive.in>
2024-10-06 11:51:36 +0530

    first

---
+++ MediaBrowser001/app/src/main/java/com/example/mediabrowserservice/service/PlaybackInfoListener.java
@@ -0,0 +1,17 @@
+package com.example.mediabrowserservice.service;
+
+import android.support.v4.media.session.PlaybackStateCompat;
+import android.support.v4.media.session.MediaSessionCompat;
+
+
+/**
+ * Listener to provide state updates from {@link MediaPlayerAdapter} (the media player)
+ * to {@link MusicService} (the service that holds our {@link MediaSessionCompat}.
+ */
+public abstract class PlaybackInfoListener {
+
+    public abstract void onPlaybackStateChange(PlaybackStateCompat state);
+
+    public void onPlaybackCompleted() {
+    }
+}
